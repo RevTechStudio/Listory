dc4b3e5bc163b1884e3fe8ca8ace377a7de3b815
RevTechStudio <240102615+RevTechStudio@users.noreply.github.com>
2025-11-04 00:23:25 +0000

    feat: Update CheckList and CheckListStructure models

--- src/Listory.Core/Models/CheckList.cs
+++ src/Listory.Core/Models/CheckList.cs
@@ -8,5 +8,6 @@ public class CheckList
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public bool IsTemplate { get; set; } = false;
     public ICollection<CheckListStructure> Structures { get; set; } = [];
 }
